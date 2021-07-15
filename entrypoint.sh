332c9e80abdcfb51f13f3ede91f5fbe855b1a3f8
giohappy <giohappy@gmail.com>
2021-07-15 15:36:23 +0200

    backport f15b858bbf152785fd41f288a4247a3c3d934065

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# Exit script in case of error
 set -e
+
+INVOKE_LOG_STDOUT=${INVOKE_LOG_STDOUT:-FALSE}
+invoke () {
+    if [ $INVOKE_LOG_STDOUT = 'true' ] || [ $INVOKE_LOG_STDOUT = 'True' ]
+    then
+        /usr/local/bin/invoke $@
+    else
+        /usr/local/bin/invoke $@ > /usr/src/nexus/invoke.log 2>&1
+    fi
+    echo "$@ tasks done"
+}
 
 # Start cron && memcached services
 service cron restart
@@ -10,11 +23,15 @@ echo "-----------------------------------------------------"
 echo "STARTING DJANGO ENTRYPOINT $(date)"
 echo "-----------------------------------------------------"
 
-/usr/local/bin/invoke update >> /usr/src/nexus/invoke.log
+invoke update
 
 source $HOME/.bashrc
 source $HOME/.override_env
 
+
+echo DOCKER_API_VERSION=$DOCKER_API_VERSION
+echo POSTGRES_USER=$POSTGRES_USER
+echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 echo DATABASE_URL=$DATABASE_URL
 echo GEODATABASE_URL=$GEODATABASE_URL
 echo SITEURL=$SITEURL
@@ -25,12 +42,9 @@ echo MONITORING_HOST_NAME=$MONITORING_HOST_NAME
 echo MONITORING_SERVICE_NAME=$MONITORING_SERVICE_NAME
 echo MONITORING_DATA_TTL=$MONITORING_DATA_TTL
 
-/usr/local/bin/invoke waitfordbs >> /usr/src/nexus/invoke.log
-echo "waitfordbs task done"
+invoke waitfordbs
 
-echo "running migrations"
-/usr/local/bin/invoke migrations >> /usr/src/nexus/invoke.log
-echo "migrations task done"
+invoke migrations
 
 cmd="$@"
 
@@ -46,33 +60,29 @@ else
         echo "Executing Celery server $cmd for Production"
     else
 
-        /usr/local/bin/invoke prepare >> /usr/src/nexus/invoke.log
-        echo "prepare task done"
+        invoke prepare
 
-        if [ ${IS_FIRST_START} = "true" ] || [ ${IS_FIRST_START} = "True" ] || [ ${FORCE_REINIT} = "true" ]  || [ ${FORCE_REINIT} = "True" ] || [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
-            /usr/local/bin/invoke updategeoip >> /usr/src/nexus/invoke.log
-            echo "updategeoip task done"
-            /usr/local/bin/invoke fixtures >> /usr/src/nexus/invoke.log
-            echo "fixture task done"
-            /usr/local/bin/invoke monitoringfixture >> /usr/src/nexus/invoke.log
-            echo "monitoringfixture task done"
-            /usr/local/bin/invoke initialized >> /usr/src/nexus/invoke.log
-            echo "initialized"
+        if [ ${FORCE_REINIT} = "true" ]  || [ ${FORCE_REINIT} = "True" ] || [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
+            invoke updategeoip
+            invoke fixtures
+            invoke monitoringfixture
+            invoke initialized
         fi
 
-        echo "refresh static data"
-        /usr/local/bin/invoke statics >> /usr/src/nexus/invoke.log
-        echo "static data refreshed"
-        /usr/local/bin/invoke waitforgeoserver >> /usr/src/nexus/invoke.log
-        echo "waitforgeoserver task done"
-        /usr/local/bin/invoke geoserverfixture >> /usr/src/nexus/invoke.log
-        echo "geoserverfixture task done"
-        /usr/local/bin/invoke updateadmin >> /usr/src/nexus/invoke.log
-        echo "updateadmin task done"
+        invoke statics
+        invoke waitforgeoserver
+        invoke geoserverfixture
+        invoke updateadmin
 
         cmd=$UWSGI_CMD
         echo "Executing UWSGI server $cmd for Production"
     fi
 fi
+
+echo "-----------------------------------------------------"
+echo "FINISHED DJANGO ENTRYPOINT --------------------------"
+echo "-----------------------------------------------------"
+
+# Run the CMD 
 echo "got command $cmd"
 exec $cmd
